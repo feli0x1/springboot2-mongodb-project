7743b650708c5fa2fe710517c49f07c9d7f7d715
feli0x1 <sdiylaogzbhk@proton.me>
2023-09-09 14:34:21 -0300

    Initial commit

---
+++ src/main/java/com/project/springboot2/mongodb/ProjectSpringboot2MongodbApplication.java
@@ -0,0 +1,13 @@
+package com.project.springboot2.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringboot2MongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringboot2MongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/springboot2/mongodb/ProjectSpringboot2MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springboot2.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringboot2MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
