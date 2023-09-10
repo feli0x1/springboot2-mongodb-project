2feb3fb3a3e03d79562bcc0b9db0dcc8abe8e732
feli0x1 <sdiylaogzbhk@proton.me>
2023-09-10 14:08:53 -0300

    User insertion implemented

--- src/main/java/com/project/springboot2/mongodb/services/UserService.java
+++ src/main/java/com/project/springboot2/mongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.springboot2.mongodb.domain.User;
+import com.project.springboot2.mongodb.dto.UserDTO;
 import com.project.springboot2.mongodb.repository.UserRepository;
 import com.project.springboot2.mongodb.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,13 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
+	
 }
