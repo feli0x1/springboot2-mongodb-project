f1874fa4e9203a65cfe6d59826eb49dbcb7faf58
feli0x1 <sdiylaogzbhk@proton.me>
2023-09-11 15:10:23 -0300

    method findById() implemented both for PostResource and PostService

---
+++ src/main/java/com/project/springboot2/mongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.project.springboot2.mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.springboot2.mongodb.domain.Post;
+import com.project.springboot2.mongodb.repository.PostRepository;
+import com.project.springboot2.mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+		
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
